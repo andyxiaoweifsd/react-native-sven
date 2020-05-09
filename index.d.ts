fc66ed3db021d63be4d13e765a51062a1f678076
张小威 <zhangxiaowei@sven-2.local>
2020-05-10 00:27:12 +0800

    11

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+/*
+ * @Date: 2020-05-09 19:57:28
+ * @LastEditors: Save
+ * @LastEditTime: 2020-05-10 00:18:41
+ * @FilePath: /index.d.ts
+ * @Description: 
+ */
+
+export * from './src/lib/base-component'
+export * from './src/lib/base-loading'
