d065c96d911d27542ad521bcd3d78f2b583d7427
张小威 <zhangxiaowei@sven-2.local>
2020-05-28 01:46:49 +0800

    更新小bug

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-05-28 01:40:29
  * @LastEditors: Save
- * @LastEditTime: 2020-05-28 01:40:48
+ * @LastEditTime: 2020-05-28 01:46:22
  * @FilePath: /index.ts
  * @Description: 
  */ 
@@ -12,7 +12,8 @@
 
 // export default RNReactNativeSven;
 
+import * as SvenStyle from './src/assets/style'
 export * from './src/lib'
 export * from './src/api'
 export * from './src/util'
-export * as SvenStyle from './src/assets/style'
+export { SvenStyle }
